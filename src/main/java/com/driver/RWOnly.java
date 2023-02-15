49a204e87f0e54c8ce38f11530b4bb90206f2d7e
Avishek <savishek@gmail.com>
2023-02-16 03:12:44 +0530

    solution3

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
-public class RWOnly {
-    
+public class RWOnly{
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
